baed8aedeb11df8414b2cc8c2e21592e14d39d14
Pixtane <94226146+Pixtane@users.noreply.github.com>
2023-12-06 21:15:56 +0200

    Update currency-exchange-script.js

--- scripts/currency-exchange-script.js
+++ scripts/currency-exchange-script.js
@@ -78,7 +78,7 @@ function updateRates(inputElement) {
     return;
   }
 
-  fetch("http://pixtane.vsevolod.pp.ua/exchange-rates")
+  fetch("https://pixtane.vsevolod.pp.ua/exchange-rates")
     .then((response) => {
       // Check if the response status is OK (200)
       if (!response.ok) {
